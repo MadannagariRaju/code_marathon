18b3aa49c12ab2b95a2a73f6c1a58616466177ad
raju <madannagariraju@gmail.com>
2026-09-05 00:47:25 +0530

    04-09-2026

--- 04-08-2026/Array-L1/RightRotateArrayByOnePlace.java
+++ 04-08-2026/Array-L1/RightRotateArrayByOnePlace.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class RightRotateArrayByOnePlace {
     public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+        int n = arr.length;
+        int temp = arr[n-1];
         
+        for(int i=n-2;i>=0;i--) {
+            arr[i+1] = arr[i];
+        }
+        arr[0] = temp;
+
+        System.out.println(Arrays.toString(arr));
     }
 }
